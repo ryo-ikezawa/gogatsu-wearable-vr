4ca737dc86d3f0dd9386b4745ab8acf02a575b2f
ikezawa.ryo <kawryo3726@gmail.com>
2019-01-31 13:37:31 +0900

    テスト

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "matrix.h"
+
+double lagrange(double x, matrix points);
+
+double lagrange(double x, matrix points)
+{
+  int i,j;
+  double temp;
+  double p=0;
+  for(i=1;i<=points.row;i++){
+    temp=1;
+    for(j=1;j<=points.row;j++){
+      if(j != i){
+	temp *= (x-mat_elem(points,j,1))/(mat_elem(points,i,1)-mat_elem(points,j,1));
+      }
+    }
+    p += temp * mat_elem(points,i,2);
+  }
+  return p;
+
+}
+
