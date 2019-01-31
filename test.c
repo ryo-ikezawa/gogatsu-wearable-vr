670d1b602c653a9e594f1301e3eb52de2b9029dd
中川潤哉 <sterben0524@gmail.com>
2019-01-31 14:31:06 +0900

    j=5

--- test.c
+++ test.c
@@ -9,7 +9,7 @@ double lagrange(double x, matrix points)
   double p=100;
   for(i=1;i<=points.row;i++){
     temp=4;
-    for(j=1;j<=points.row;j++){
+    for(j=5;j<=points.row;j++){
       if(j != i){
 	temp *= (x-mat_elem(points,j,1))/(mat_elem(points,i,1)-mat_elem(points,j,1));
       }
